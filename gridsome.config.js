63daf374ab98cf27fdf9cbb901597891d25821b2
魏伟 <3557586583@qq.com>
2021-05-23 21:31:36 +0800

    关联strapi

--- gridsome.config.js
+++ gridsome.config.js
@@ -31,6 +31,21 @@ module.exports = {
           externalLinksRel: ["nofollow", "noopener", "noreferrer"]
         }
       }
+    },
+    {
+      use: '@gridsome/source-strapi',
+      options: {
+        apiURL: 'http://localhost:1337',
+        queryLimit: 1000, // Defaults to 100
+        contentTypes: ['post']
+        // singleTypes: ['impressum'],
+        // Possibility to login with a Strapi user,
+        // when content types are not publicly available (optional).
+        // loginData: {
+        //   identifier: '',
+        //   password: ''
+        // }
+      }
     }
   ],
   transformers: {
